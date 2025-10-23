a27e49a35a089a424f11d2a3f6605a73db907eec
mv-vinycius <marcosviniciusturista@gmail.com>
2025-10-23 01:09:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # logica-com-python
+
 Exercícios e desafios de lógica de programação usando Python — com foco em raciocínio, estruturas condicionais e laços.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Se clicou no link e viu esta mensagem, tira um PRINT e me envie no Whatsapp. Preciso saber se está funcionando!</h1>
+  return (
+    <h1>
+      Se clicou no link e viu esta mensagem, tira um PRINT e me envie no
+      Whatsapp. Preciso saber se está funcionando!
+    </h1>
+  );
 }
 
 export default Home;
